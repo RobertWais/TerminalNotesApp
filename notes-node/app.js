b1795a62ade1631f6f67967e6d4186b055d1fa00
RobertWais <wais.robert@uwlax.edu>
2018-08-17 21:52:54 -0500

    Saving multiple notes

--- notes-node/app.js
+++ notes-node/app.js
@@ -16,7 +16,16 @@ console.log("Command: ",command);
 //prints entire object
 // console.log("Yargs", argv);
 if (command === "add"){
-    notes.addNote(argv.title,argv.body);
+    var note = notes.addNote(argv.title,argv.body);
+
+    if (note){
+        console.log(`Message Saved ${note.title}`)
+
+    }else{
+        console.log("Title already exists")
+    }
+
+
 }else if(command === "list"){
     notes.getAll();
 }else if(command === "read"){
